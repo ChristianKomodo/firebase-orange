e54335cef35cf96d7f3db581582dd5d14932eaf0
Christian Knoll <chris@sectorfive.com>
2023-12-03 19:59:08 -0500

    Save and load a Collection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,59 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { Firestore, addDoc, collection } from '@angular/fire/firestore';
+import { DocumentData, Firestore, QuerySnapshot, addDoc, collection, collectionData, doc, getDoc, getDocs } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+
+interface Item {
+  text: string
+};
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: `
+  <ul>
+    <li *ngFor="let item of item$ | async">
+      {{ item.text }}
+    </li>
+  </ul>
+  `
 })
 export class AppComponent implements OnInit {
-  title = 'firebase-orange';
+  // item$: Observable<Item[]>;
+  // item$ = collectionData<Item[]>(itemCollection);
+  item$: Observable<any>;
 
-  constructor(private firestore: Firestore) { }
+  constructor(private firestore: Firestore) {
+    const itemCollection = collection(this.firestore, 'test');
+    this.item$ = collectionData(itemCollection);
+  }
 
   ngOnInit(): void {
     const testCollection = collection(this.firestore, 'test');
-    addDoc(testCollection, {'text': 'Love it or hate it'});
+
+    // Add a document with an auto-generated id
+    addDoc(testCollection, { 'text': 'first one' }).then((docRef) => {
+      console.log('Document written with ID: ', docRef.id);
+    });
+
+    const oneDoc = getDoc(doc(testCollection, 'Y1O7RCJcpKwPJ0b28w2o')).then((doc) => {
+      if (doc.exists()) {
+        console.log('Document data:', doc.data());
+      } else {
+        // if doc.data() is undefined
+        console.log('No such document!');
+      }
+    });
+    console.log('oneDoc', oneDoc);
+
+    const docs = getDocs(testCollection);
+    docs.then((docs: QuerySnapshot<DocumentData>) => {
+      docs.forEach((doc) => {
+        console.log(doc.id, ' => ', doc.data());
+      });
+    });
+
   }
 }
