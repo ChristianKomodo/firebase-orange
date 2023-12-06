c9454af1a1039884ae7af9e962af85cfcd6991e6
Christian Knoll <chris@sectorfive.com>
2023-12-06 21:16:00 +0000

    using docs code for auth

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,8 +1,7 @@
-import { Component, OnDestroy, OnInit, inject } from '@angular/core';
+import { Component, Optional } from '@angular/core';
+import { Auth, authState, signOut, User } from '@angular/fire/auth';
+import { EMPTY, Observable, Subscription } from 'rxjs';
 import { CommonModule } from '@angular/common';
-import { Auth, User, user, authState } from '@angular/fire/auth';
-import { Subscription } from 'rxjs';
-import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-user',
@@ -11,29 +10,75 @@ import { UserService } from '../user.service';
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.scss']
 })
-export class UserComponent implements OnInit {
-  private auth: Auth = inject(Auth);
-  userEmail: string | undefined | null = null;
-  user: User | null = null;
+export class UserComponent {
 
-  // The `user` observable streams events triggered by sign-in, sign-out, and token refresh events.
-  user$ = user(this.auth);
+  public readonly user: Observable<User | null> = EMPTY;
 
-  constructor(private userService: UserService) {
-    // this.user = this.auth.currentUser;
-    // console.log('----------- this.user is', this.user);   // null
+  showLoginButton = false;
+  showLogoutButton = false;
+
+  constructor(
+    @Optional() private auth: Auth
+  ) {
+    if (auth) {
+      this.user = authState(this.auth);
+    }
   }
 
-  ngOnInit(): void {
-    // console.log('ng this.auth.currentUser is', this.auth.currentUser);   // null
-  }
+  ngOnInit(): void { }
 
-  clicked() {
-    console.log('clicked!');
-  }
-
-  logOut() {
-    this.userService.signOut();
-  }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { Component, OnDestroy, OnInit, inject } from '@angular/core';
+// import { CommonModule } from '@angular/common';
+// import { Auth, User, user, authState } from '@angular/fire/auth';
+// import { Subscription } from 'rxjs';
+// import { UserService } from '../user.service';
+
+// @Component({
+//   selector: 'app-user',
+//   standalone: true,
+//   imports: [CommonModule],
+//   templateUrl: './user.component.html',
+//   styleUrls: ['./user.component.scss']
+// })
+// export class UserComponent implements OnInit {
+//   private auth: Auth = inject(Auth);
+//   userEmail: string | undefined | null = null;
+//   user: User | null = null;
+
+//   // The `user` observable streams events triggered by sign-in, sign-out, and token refresh events.
+//   user$ = user(this.auth);
+
+//   constructor(private userService: UserService) {
+//     // this.user = this.auth.currentUser;
+//     // console.log('----------- this.user is', this.user);   // null
+//   }
+
+//   ngOnInit(): void {
+//     // console.log('ng this.auth.currentUser is', this.auth.currentUser);   // null
+//   }
+
+//   clicked() {
+//     console.log('clicked!');
+//   }
+
+//   logOut() {
+//     this.userService.signOut();
+//   }
+
+// }
