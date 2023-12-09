722452093442415909887cd1476b6a3988c78723
Christian Knoll <chris@sectorfive.com>
2023-12-08 19:12:14 -0500

    loading user data from logged in user

--- src/app/firestore/firestore.component.ts
+++ src/app/firestore/firestore.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, inject } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { DocumentData, Firestore, QuerySnapshot, addDoc, collection, collectionData, doc, getDoc, getDocs } from '@angular/fire/firestore';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { DataFormComponent } from './data-form/data-form.component';
 import { Auth, user } from '@angular/fire/auth';
 
@@ -15,8 +15,11 @@ import { Auth, user } from '@angular/fire/auth';
 })
 export class FirestoreComponent implements OnInit {
     users$: Observable<any>;
-    userData$!: Observable<any>;
-    userDisplayData: any;
+
+    // User data
+    private userDataSubject = new BehaviorSubject<any | null>(null);
+    userData$ = this.userDataSubject.asObservable();
+
     dataForm!: FormGroup
     // Authenticated user
     private auth: Auth = inject(Auth);
@@ -29,7 +32,6 @@ export class FirestoreComponent implements OnInit {
 
     ngOnInit(): void {
         // Get the user's data from the users collection
-        // First get the current user's UID
         const uuid = this.user$.subscribe((user) => {
             if (user) {
                 // User is signed in, see docs for a list of available properties
@@ -37,25 +39,28 @@ export class FirestoreComponent implements OnInit {
                 const uid = user.uid;
                 console.log('uid for fetching user data is', uid);
                 // Get the user's data from the users collection
-                getDoc(doc(this.firestore, 'users', uid)).then((doc) => {
-                    if (doc.exists()) {
-                        console.log('Document data:', doc.data());
-                        this.userData$ = doc.data();
-                        // this.displayData(doc.data());
-                    } else {
-                        // if doc.data() is undefined
-                        console.log('No documents for this user!');
-                    }
-                });
+                getDoc(doc(this.firestore, 'users', uid))
+                    .then((doc) => {
+                        if (doc.exists()) {
+                            this.userDataSubject.next(doc.data()); // Set doc.data() to userData$
+                        } else {
+                            console.log("No such document!");
+                        }
+                    }).catch((error) => {
+                        console.log("Error getting document:", error);
+                    });
+                // // use this to add example user data to this user
+                //this.populateNewUserData(uid);
             } else {
                 console.log('user is not signed in');
             }
         });
-        // const userData = getDoc
     }
 
-    displayData(data: any) {
-        console.log('user data:', data.userData);
-        this.userDisplayData = data.userData;
+    populateNewUserData(uid: string) {
+        // addDoc to this user's userData
+        addDoc(collection(this.firestore, 'users', uid, 'userData'), {
+            movies: ['movie1', 'movie2', 'movie3']
+        });
     }
 }
