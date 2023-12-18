01a8ff74ea2ffbcfee2a7e7d706dd197c6e6caeb
Christian Knoll <chris@sectorfive.com>
2023-12-18 14:10:53 -0500

    Displaying list of movies for logged in user

--- src/app/user-data/user-data.component.ts
+++ src/app/user-data/user-data.component.ts
@@ -1,12 +1,18 @@
+import { Firestore, collection, collectionData, addDoc, CollectionReference, DocumentReference, getDoc, doc, getDocs } from '@angular/fire/firestore';
 import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Firestore, collection, collectionData, doc, getDoc, setDoc } from '@angular/fire/firestore';
-import { BehaviorSubject, Observable } from 'rxjs';
-import { Auth, user } from '@angular/fire/auth';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { Auth, user } from '@angular/fire/auth';
+import { query } from '@angular/animations';
 
 interface UserProfile {
   email: string;
+}
+interface Movie {
+  title: string;
+  year: string;
+  omdbid: string;
 }
 
 @Component({
@@ -17,28 +23,25 @@ interface UserProfile {
   styleUrls: ['./user-data.component.scss']
 })
 export class UserDataComponent implements OnInit {
-  private auth: Auth = inject(Auth);
   private firestore: Firestore = inject(Firestore); // inject Cloud Firestore
   users$: Observable<UserProfile[]>;
+  moviesCollection!: CollectionReference;
+  private auth: Auth = inject(Auth);
   user$ = user(this.auth);
-  private userDataSubject = new BehaviorSubject<any | null>(null);
-  userData$ = this.userDataSubject.asObservable();
-  movieDataForm!: FormGroup;
-  movieData!: string[];
-  uid: string = '';
+  uid!: string;
+  someMovies: Movie[] = [];
+  movieForm!: FormGroup;
 
   constructor(private fb: FormBuilder) {
     // get a reference to the user-profile collection
     const userProfileCollection = collection(this.firestore, 'users');
-
     // get documents (data) from the collection using collectionData
     this.users$ = collectionData(userProfileCollection) as Observable<UserProfile[]>;
   }
 
   ngOnInit(): void {
     this.setUpForms();
-
-    // Get the user
+    // get the uid of the current logged-in user
     this.user$.subscribe((user) => {
       if (user) {
         // User is signed in, see docs for a list of available properties
@@ -49,49 +52,34 @@ export class UserDataComponent implements OnInit {
           return;
         }
         console.log('uid for fetching user data is', this.uid);
-
-        // Get the user's data from the users collection
-        getDoc(doc(this.firestore, 'users', this.uid))
-          .then((doc) => {
-            if (doc.exists()) {
-              console.log("Found UserData Document:", doc.data());
-              const someData = doc.data();
-              console.log('someData', someData);
-              this.userDataSubject.next(doc.data()); // Set doc.data() to userData$
-              this.movieData = someData['movies'] || [];
-              console.log('setting movieData', this.movieData);
-            } else {
-              console.log("No such document!");
-            }
-          }).catch((error) => {
-            console.log("Error getting document:", error);
+        // get the "movies" collection within the user's document
+        const moviesCollection = collection(this.firestore, 'users', this.uid, 'movies');
+        getDocs(moviesCollection).then((querySnapshot) => {
+          querySnapshot.forEach((doc) => {
+            const title = doc.data()['title'];
+            const year = doc.data()['year'];
+            const omdbid = doc.data()['omdbid'];
+            this.someMovies.push({ title: title, year: year, omdbid: omdbid });
+            console.log('someMovies', this.someMovies);
           });
-
-      } else {
-        console.error('user is not signed in');
-        return;
+        });
       }
     });
   }
 
   setUpForms() {
-    this.movieDataForm = this.fb.group({
-      title: ['', Validators.required]
+    this.movieForm = this.fb.group({
+      movie: ['', Validators.required]
     });
   }
 
-  updateUserData() {
-    const userDoc = doc(this.firestore, 'users', this.uid);
-    const formData = this.movieDataForm.value;
-    console.log('formData', formData);
-    const updatedMovieData = { movies: [...this.movieData, formData.movie] };
-    console.log('updatedMovieData', updatedMovieData);
-    console.log('form deactivated - need to redo this page');
-    return;
-    setDoc(userDoc, updatedMovieData, { merge: true }).then(() => {
-      console.log('Movie data updated');
-    }).catch((error) => {
-      console.error('Error updating user data:', error);
-    });
+  addMovie(movie: Movie): void {
+    // const movie: Movie = { title, year, omdbid };
+    if (!movie.title || !movie.year || !movie.omdbid) {
+      console.log('missing movie data');
+      return
+    }
+    console.log('adding movie', movie);
+    addDoc(collection(this.firestore, 'users', this.uid, 'movies'), movie);
   }
 }
--- src/app/your-movies/your-movies.component.ts
+++ src/app/your-movies/your-movies.component.ts
@@ -1,5 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Firestore, collection, collectionData } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+
+interface UserProfile {
+  email: string;
+}
 
 @Component({
   selector: 'app-your-movies',
@@ -9,5 +15,15 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./your-movies.component.scss']
 })
 export class YourMoviesComponent {
+  private firestore: Firestore = inject(Firestore); // inject Cloud Firestore
+  users$: Observable<UserProfile[]> | undefined;
+
+  constructor() {
+    // get a reference to the user-profile collection
+    const userProfileCollection = collection(this.firestore, 'users');
+
+    // get documents (data) from the collection using collectionData
+    this.users$ = collectionData(userProfileCollection) as Observable<UserProfile[]>;
+  }
 
 }
