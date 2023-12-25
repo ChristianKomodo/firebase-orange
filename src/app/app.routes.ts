bdaff6ea15b314db94e84c50a47b89d334195313
Christian Knoll <chris@sectorfive.com>
2023-12-24 21:30:00 -0500

    buttons, cleanup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,11 @@ import { canActivate, redirectLoggedInTo, redirectUnauthorizedTo } from '@angula
 
 import { HomeComponent } from './home/home.component';
 import { UserFormComponent } from './user-form/user-form.component';
-import { FirestoreComponent } from './firestore/firestore.component';
 import { UserDataComponent } from './user-data/user-data.component';
 import { YourMoviesComponent } from './your-movies/your-movies.component';
 
 // redirects logged in users
-const redirectLoggedInToFirestore = () => redirectLoggedInTo(['firestore']);
+const redirectLoggedInToFirestore = () => redirectLoggedInTo(['home']);
 // redirects not-logged in users
 const redirectUnauthorizedToLogin = () => redirectUnauthorizedTo(['user-form']);
 
@@ -19,7 +18,6 @@ export const routes: Routes = [
   // The below long-hand version doesn't work because it can't find [AngularFireAuthGuard] and it's not an import
   // { path: 'login', component: FirestoreComponent, canActivate: [AngularFireAuthGuard], data: { authGuardPipe: redirectUnauthorizedToLogin } },
   // Instead, use this short-hand version:
-  { path: 'firestore', component: FirestoreComponent, ...canActivate(redirectUnauthorizedToLogin) },
   { path: 'user-data', component: UserDataComponent, ...canActivate(redirectUnauthorizedToLogin) },
   { path: 'reviews', component: YourMoviesComponent, ...canActivate(redirectUnauthorizedToLogin) },
 ];
